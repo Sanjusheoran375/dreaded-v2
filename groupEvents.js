ea5ea18546bd504ad93e97eeadf5accd8f4abd99
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-09-08 21:15:47 +0530

    Update groupEvents.js

--- groupEvents.js
+++ groupEvents.js
@@ -1,5 +1,5 @@
 const events = process.env.EVENTS || 'true';
-const botname = process.env.BOTNAME || 'DREADED';
+const botname = process.env.BOTNAME || 'SANJU THE ROCK';
 
 const Events = async (client, Fortu) => {
     const Myself = await client.decodeJid(client.user.id);
@@ -21,7 +21,7 @@ const Events = async (client, Fortu) => {
             if (Fortu.action == "add") {
                 let userName = num;
 
-                let Welcometext = ` Holla @${userName.split("@")[0]} 👋\n\nWelcome to ${metadata.subject}.\n\nGroup Description:-  ${desc}\n\nThank You.\n\nThis is an automated message sent by ${botname} via baileys.`;
+                let Welcometext = ` Hello @${userName.split("@")[0]} 👋\n\nWelcome to ${metadata.subject}.\n\nGroup Description:-  ${desc}\n\nThank You.\n\nThis is an automated message sent by ${botname} via baileys.`;
                 if (events === 'true') {
                     await client.sendMessage(Fortu.id, {
                         image: { url: dpuser },
